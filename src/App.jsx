9d852dd620fb4139e4f20c8e8114264ff03f1e01
Umasri537 <gubbalaumasri4@gmail.com>
2025-09-18 13:41:30 +0530

    For Randomnum

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,20 @@ import { useEffect, useState } from "react"
 
 
 function App() {
-   var [count,setCount]=useState(0);
+   var [num,setNum]=useState(0);
 
      useEffect(()=>{
       setInterval(()=>{
-        setCount((cv)=>{
-          return cv+1;
-        });
-        console.log(count);
+         var n=Math.random();
+         console.log(n);
+         setNum(n)
       },2000); 
      },[])
    
   return (
     <div className="border border-2 border-success m-3 p-3">
        <h1>React Component App</h1> 
-        <h3 className="border border-1 border-warning m-2 p-2">Counter:{count}</h3>
+        <h3 className="border border-1 border-warning m-2 p-2">RandomNumber:{num}</h3>
     </div>
       
   )
