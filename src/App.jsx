cf441d3e4ab356286e27b04416541fbdafbd750c
Umasri537 <gubbalaumasri4@gmail.com>
2025-09-18 13:48:35 +0530

    for Counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 
 function App() {
-   var [num,setNum]=useState(0);
-
+   var [count,setCount]=useState(0);
+  var cRef=useRef();
      useEffect(()=>{
-      setInterval(()=>{
-         var n=Math.random();
-         console.log(n);
-         setNum(n)
+      cRef.current=setInterval(()=>{
+        setCount((cv)=>{
+          return cv+1;
+        });
+        console.log(count);
       },2000); 
      },[])
+     useEffect(()=>{
+       if(count===5){
+          clearInterval(cRef.current)
+       }
+     },[count])
    
   return (
     <div className="border border-2 border-success m-3 p-3">
        <h1>React Component App</h1> 
-        <h3 className="border border-1 border-warning m-2 p-2">RandomNumber:{num}</h3>
+        <h3 className="border border-1 border-warning m-2 p-2">Counter:{count}</h3>
     </div>
       
   )
