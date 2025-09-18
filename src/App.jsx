ae3cc2030cfa82154278fbc318c76a466c5a6d44
Umasri537 <gubbalaumasri4@gmail.com>
2025-09-18 13:11:33 +0530

    ReactPractiseApp

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react"
+
+
+function App() {
+   var [num,setNum]=useState();
+
+  //  useEffect(()=>{
+      setInterval(()=>{
+        var n=Math.random();
+        console.log(n);
+        setNum(n)
+      },2000)
+  //  },[])
+   
+  return (
+    <div className="border border-2 border-success m-3 p-3">
+       <h1>React Component App</h1> 
+        <h3 className="border border-1 border-warning m-2 p-2">RandomNumber:{num}</h3>
+    </div>
+      
+  )
+}
+
+export default App
